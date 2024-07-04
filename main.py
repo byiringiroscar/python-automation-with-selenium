e3684f53c66841c0e4daa3b69a7c39344f7086d3
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-07-04 18:43:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
@@ -10,10 +12,21 @@ driver = webdriver.Chrome(service=service)
 
 driver.get("https://google.com")
 
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "gLFy"))
+)
+
+
 input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
 input_element.clear()
-input_element.send_keys("lionel messi" + Keys.ENTER)
+input_element.send_keys("tech with tim" + Keys.ENTER)
 
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Tech With Tim"))
+)
+
+link = driver.find_element(By.PARTIAL_LINK_TEXT, "Tech With Tim") # here is to check the first link which atleast contain tech with tim text
+link.click()
 
 
 
