0d8a9bf5a44364d8957580d6e7bd0d3c4276f9b8
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-07-04 18:19:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import time
 
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
+
+driver.get("https://google.com")
+
+
+time.sleep(10)
+
+driver.quit()
