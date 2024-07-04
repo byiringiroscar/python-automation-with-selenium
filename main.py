43197cf312df7a45001585a5a312dc443682862b
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-07-04 18:25:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 
@@ -10,7 +11,9 @@ driver = webdriver.Chrome(service=service)
 driver.get("https://google.com")
 
 input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
-input_element.send_keys("lionel messi")
+input_element.send_keys("lionel messi" + Keys.ENTER)
+
+
 
 
 time.sleep(10)
