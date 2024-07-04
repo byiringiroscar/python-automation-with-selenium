f12f57e7c505a7c37758c1494c1979434f48af74
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-07-04 22:54:45 +0200

    Update cookieclickers.py

--- cookieclickers.py
+++ cookieclickers.py
@@ -10,4 +10,25 @@ import time
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
+
+
 driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+
+search_bar = WebDriverWait(driver, 20).until(
+    EC.element_to_be_clickable((By.CLASS_NAME, "langSelectButton"))
+)
+
+choose_lang = driver.find_element(By.CLASS_NAME, "langSelectButton")
+choose_lang.click()
+
+
+cookie_id = "bigCookie"
+
+cookie = driver.find_element(By.ID, cookie_id)
+cookie.click()
+
+
+time.sleep(5)
+
+driver.quit()
