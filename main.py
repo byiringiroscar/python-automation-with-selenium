dbbfc4d9d15c39d5e5cf2a2a8a162b356293c860
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-07-04 18:23:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import time
 
 
@@ -7,6 +8,9 @@ service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://google.com")
+
+input_element = driver.get_element(By.CLASS_NAME, "gLFyf")
+
 
 
 time.sleep(10)
